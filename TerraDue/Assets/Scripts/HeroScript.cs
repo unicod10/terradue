93cf130e9dfb8850b6ba36a7ea84d19e5888f35e
Alessio Franchini <alessio.franchini1@studenti.unimi.it>
2017-04-21 12:40:40 +0200

    Initial Commit
    First implementation of lobby.

---
+++ TerraDue/Assets/Scripts/HeroScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class HeroScript : NetworkBehaviour {
+
+    public Transform spawnPoint;
+
+    void Update()
+    {
+        if (!isLocalPlayer)
+            return;
+
+        var x = Input.GetAxis("Horizontal") * 0.1f;
+        var z = Input.GetAxis("Vertical") * 0.1f;
+
+        transform.Translate(x, 0, z);
+    }
+}
---
+++ TerraDue/Assets/Scripts/MyLobbyManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using Prototype.NetworkLobby;
+
+public class MyLobbyManager : LobbyManager {
+
+    private const int ALIEN_PREFAB_OFFSET = 3;
+
+    private int spawnedHumans = 0;
+    private int spawnedAliens = 0;
+
+    // https://forum.unity3d.com/threads/which-function-to-override.391076/#post-2571472
+    public override GameObject OnLobbyServerCreateGamePlayer(NetworkConnection conn, short playerControllerId)
+    {
+        Transform spawnPoint;
+        GameObject heroPrefab;
+        GameObject player;
+
+        // Spawn at hero's unique position
+        if (spawnedHumans == spawnedAliens)
+        {
+            var playerIndex = ++spawnedHumans;
+            spawnPoint = GameObject.Find("SpawnHuman" + playerIndex).transform;
+            heroPrefab = spawnPrefabs[playerIndex - 1];
+        }
+        else
+        {
+            var playerIndex = ++spawnedAliens;
+            spawnPoint = GameObject.Find("SpawnAlien" + playerIndex).transform;
+            heroPrefab = spawnPrefabs[playerIndex - 1 + ALIEN_PREFAB_OFFSET];
+        }
+
+        player = Instantiate(heroPrefab, spawnPoint.position, Quaternion.identity);
+        player.GetComponent<HeroScript>().spawnPoint = spawnPoint;
+        return player;
+    }
+}
