618a8e3eb5d853df798d70d248407ba748f4ea72
Jacopo <from.the.darkest.past@gmail.com>
2017-05-14 22:34:18 +0200

    Initial UI

--- TerraDue/Assets/Scripts/MinionBehaviour.cs
+++ TerraDue/Assets/Scripts/MinionBehaviour.cs
@@ -5,6 +5,11 @@ public class MinionBehaviour : NetworkBehaviour {
 
     public GameObject groupLeader;
     public int groupId;
+    /*
+    * Lane 1 is on the left for humans and on the right for aliens
+    * Lane 2 is on the center
+    * Lane 3 is on the right for humans and on the left for aliens
+    */
     public int lane;
 
     void Update ()
