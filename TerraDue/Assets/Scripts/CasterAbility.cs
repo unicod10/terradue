6524cf961b7c33784148b7ffa9700862e82b8986
Istorn <istorn.one@gmail.com>
2017-05-23 10:14:37 +0200

    Ability class added
    They describe their own behaviour and cooldown rules

---
+++ TerraDue/Assets/Scripts/CasterAbility.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CasterAbility : MonoBehaviour {
+    //Selecting an opposite target, the player will move instantly  there and perform a critical attack
+    //Cooldown: 10seconds, unlock @lvl3
+    // Use this for initialization
+
+    //Parameters to check if the ability is available or not
+    private float cooldown;
+    private bool active;
+   
+    float interval = 0.1f;
+    float nextTime = 0;
+
+
+    void Start () {
+        this.cooldown = 10.0f;
+        this.active = false;
+	}
+	public bool cast()
+    {
+        if (this.cooldown == 0.0f)
+        {
+            this.cooldown = 10.0f;
+            this.active = true;
+            return this.active;
+        }
+        return this.active;
+    }
+    public void endEffect()
+    {
+        this.active = false;
+        
+    }
+	// Update is called once per frame
+	void Update () {
+
+        if (Time.time >= nextTime)
+        {
+            //If the ability is active...
+            if (this.active)
+            {
+                 this.endEffect();
+
+            }
+            //If not...
+            else
+            {
+                if (this.cooldown >= 0.0f)
+                {
+                    this.cooldown -= 0.1f;
+                }
+
+            }
+            nextTime += interval;
+        }
+    }
+}
---
+++ TerraDue/Assets/Scripts/TankAbility.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankAbility : MonoBehaviour {
+    //Attack power increased by 50% for 5 seconds, cooldown 10 seconds, unlock @lvl3
+    //Parameters to check if the ability is available or not
+    private float cooldown;
+    private bool active;
+    private float activeTime;
+   
+    float interval = 0.1f;
+    float nextTime = 0;
+    // Use this for initialization
+    void Start()
+    {
+        this.cooldown = 10.0f;
+        this.active = false;
+        this.activeTime = 0.0f;
+    }
+    public bool cast()
+    {
+        if (this.cooldown == 0.0f)
+        {
+            this.active = true;
+            this.cooldown = 10.0f;
+            return this.active;
+        }
+        return this.active;
+    }
+    public bool endEffect()
+    {
+        if (this.activeTime == 5.0f)
+        {
+            this.active = false;
+            this.activeTime = 0.0f;
+            
+            return this.active;
+        }
+        return this.active;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time >= nextTime)
+        {
+            //If the ability is active...
+            if (this.active)
+            {
+                if (this.activeTime >= 5.0f)
+                {
+                    this.endEffect();
+                    
+                }
+                else
+                {
+                    if (this.cooldown > 0.0f)
+                    {
+                        this.cooldown = 0.1f;
+                    }
+                    
+                    this.activeTime += 0.1f;
+                }
+            }
+            //If not...
+            else
+            {
+                if (this.cooldown >= 0.0f)
+                {
+                    this.cooldown-= 0.1f;
+                }
+                
+            }
+            nextTime += interval;
+        }
+    }
+
+}
