55cb8f618bc71192660f6b42bd2f1cb89277a27f
Istorn <istorn.one@gmail.com>
2017-05-27 11:54:46 +0200

    Jungle Mob: Scripts added

---
+++ TerraDue/Assets/Scripts/JungleMobSpawnBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+public class JungleMobSpawnBehaviour : NetworkBehaviour {
+
+    public GameObject JungleMobPrefab;
+    
+
+ 
+    
+    
+
+    void Start()
+    {
+        if (!isServer)
+        {
+            return;
+        }
+
+
+
+        //We spawn a group of 4 mob one time per all match
+
+        var mob = Instantiate(JungleMobPrefab, transform.position, Quaternion.identity);
+        NetworkServer.Spawn(mob);
+        NetworkServer.Spawn(mob);
+        NetworkServer.Spawn(mob);
+        NetworkServer.Spawn(mob);
+    }
+
+    void Update()
+    {
+        if (!isServer)
+        {
+            return;
+        }
+
+}
