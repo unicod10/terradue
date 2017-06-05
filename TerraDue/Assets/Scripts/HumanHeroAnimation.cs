23f377b5b2b80a7dd3aa7e9e48b9d9b8f2e3db13
Jacopo <from.the.darkest.past@gmail.com>
2017-06-05 17:56:37 +0200

    Animation basics

---
+++ TerraDue/Assets/Scripts/HumanHeroAnimation.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class HumanHeroAnimation : NetworkBehaviour, IAnimations {
+    
+    public void PlayIdle()
+    {
+        if(isLocalPlayer)
+        {
+            CmdPlayIdle();
+        }
+    }
+
+    [Command]
+    public void CmdPlayIdle()
+    {
+        RpcPlayIdle();
+    }
+
+    [ClientRpc]
+    private void RpcPlayIdle()
+    {
+        GetComponent<Animator>().Play("assault_combat_idle");
+    }
+
+    public void PlayMoving()
+    {
+        if (isLocalPlayer)
+        {
+            CmdPlayMoving();
+        }
+    }
+
+    [Command]
+    public void CmdPlayMoving()
+    {
+        RpcPlayMoving();
+    }
+
+    [ClientRpc]
+    private void RpcPlayMoving()
+    {
+        GetComponent<Animator>().Play("assault_combat_run");
+    }
+
+    public void PlayAttacking()
+    {
+        if (isLocalPlayer)
+        {
+            CmdPlayAttacking();
+        }
+    }
+
+    [Command]
+    public void CmdPlayAttacking()
+    {
+        RpcPlayAttacking();
+    }
+
+    [ClientRpc]
+    private void RpcPlayAttacking()
+    {
+        GetComponent<Animator>().Play("assault_combat_shoot");
+    }
+}
