abba31efa12d87b15ba6e910a29e955f088b9b69
unicod10 <unicod10@users.noreply.github.com>
2017-06-13 21:15:31 +0200

    JungleMob AI implemented (not finished yet)

---
+++ TerraDue/Assets/AI/Actions/JungleMinionAttackAlienHero.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using RAIN.Action;
+using RAIN.Core;
+
+[RAINAction]
+public class JungleMinionAttackAlienHero : RAINAction
+{
+	GameObject target;
+	JungleMobBehaviour action;
+	public override void Start(RAIN.Core.AI ai)
+	{
+		base.Start(ai);
+		target = ai.WorkingMemory.GetItem<GameObject>("aCloseAlienHero");
+		action = ai.Body.GetComponentInChildren<JungleMobBehaviour>();
+
+	}
+
+	public override ActionResult Execute(RAIN.Core.AI ai)
+	{
+
+		//action.Attack(target);
+		return ActionResult.SUCCESS;
+	}
+
+    public override void Stop(RAIN.Core.AI ai)
+    {
+        base.Stop(ai);
+    }
+}
---
+++ TerraDue/Assets/AI/Actions/JungleMinionAttackHumanHero.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using RAIN.Action;
+using RAIN.Core;
+
+[RAINAction]
+public class JungleMinionAttackHumanHero : RAINAction
+{
+	GameObject target;
+	JungleMobBehaviour action;
+	public override void Start(RAIN.Core.AI ai)
+	{
+		base.Start(ai);
+		target = ai.WorkingMemory.GetItem<GameObject>("aCloseHumanHero");
+		action = ai.Body.GetComponentInChildren<JungleMobBehaviour>();
+
+	}
+
+	public override ActionResult Execute(RAIN.Core.AI ai)
+	{
+		//action.Attack(target);
+		return ActionResult.SUCCESS;
+	}
+
+    public override void Stop(RAIN.Core.AI ai)
+    {
+        base.Stop(ai);
+    }
+}
